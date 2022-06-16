c87c4197f436213e0c38b4799f2e310d4d0c00c7
gya <gya747283747@outlook.com>
2022-06-16 17:51:11 +0800

    error: temp commit

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+	parser: "@typescript-eslint/parser",
+	extends: ["plugin:@typescript-eslint/recommended", "react-app", "plugin:prettier/recommended"],
+	plugins: ["@typescript-eslint", "react"],
+	rules: {},
+};
+/**
+ * reference: https://zhuanlan.zhihu.com/p/62401626
+ * 
+  1. 安装 ESLint 解析 TypeScript 的依赖： 
+    $ yarn add eslint @typescript-eslint/parser @typescript-eslint/eslint-plugin -D
+  2. 安装prettier依赖： 
+    yarn add prettier eslint-config-prettier eslint-plugin-prettier -D
+  3. Visual Studio Code 集成 ESLint 与 Prettier
+    安装vsc插件
+
+ * 
+ */
--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
-
-    const a = (a:number) => {
-    console.log('a', a);
-  }
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const a = (a: number) => {
+		console.log("a", a);
+	};
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<img src={logo} className='App-logo' alt='logo' />
+				<h1>Welcome to React Router!</h1>
+				<Routes>
+					<Route path='/' element={<Home />} />
+					<Route path='about' element={<About />} />
+				</Routes>
+			</header>
+		</div>
+	);
 }
 
 export default App;
