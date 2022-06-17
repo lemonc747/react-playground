11e57617d83196b3ac4233b08aec54ece6d22ad7
gya <gya747283747@outlook.com>
2022-06-17 14:27:36 +0800

    --build

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import CompA from './pages/a';
 import CompB from './pages/b';
 
@@ -13,7 +13,13 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
-        <h1>Welcome to React Router!</h1>
+        {/* <h1>Welcome to React Router!</h1> */}
+        <nav>
+          <Link to='/about'>About</Link>
+        </nav>
+        <nav>
+          <Link to='/'>Home</Link>
+        </nav>
         <Routes>
           <Route path='/' element={<CompA />} />
           <Route path='about' element={<CompB />} />
